bf448de72e5f401c7b63c9cb2623bcc7e367ae92
Marija Kardum <marijakardum1@gmail.com>
2019-06-23 11:15:02 +0200

    All steps included, testing ...

--- watershed_implementation.py
+++ watershed_implementation.py
@@ -52,13 +52,25 @@ sure_bg = cv2.dilate(opening, kernel, iterations=3)
 cv2.imshow("sure_bg", sure_bg)
 
 # Finding sure foreground area
-dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+sure_fg = cv2.erode(opening, kernel, iterations=3)
 cv2.imshow("sure_fg", sure_fg)
 
 # Finding unknown region
 sure_fg = numpy.uint8(sure_fg)
 unknown = cv2.subtract(sure_bg, sure_fg)
 cv2.imshow("unknown", unknown)
+
+# Marker labelling
+ret, markers = cv2.connectedComponents(sure_fg)
+
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers + 1
+
+# Now, mark the region of unknown with zero
+markers[unknown == 255] = 0
+
+markers = cv2.watershed(img, markers)
+img[markers == -1] = [255, 0, 0]
+cv2.imshow("watershed", img)
 
 cv2.waitKey(0)
